410b8ccc0ed36a8a6b6f7730a9c98a4006473009
valenoirs <luckyvw.general@gmail.com>
2022-04-22 20:51:36 +0800

    User layout and login page

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/login', (req, res) => {
+    res.render('user/login', {layout: 'layouts/user', title: 'Login'})
+})
+
 router.get('/', (req, res) => {
-    res.send('Halaman Utama User');
+    res.send('Halaman Utama User')
 })
 
 module.exports = router;
