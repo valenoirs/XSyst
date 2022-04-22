62e0ce210f0a886496c50434e51ae7db8750cc1c
valenoirs <luckyvw.general@gmail.com>
2022-04-22 20:20:20 +0800

    User model created

---
+++ models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User', new mongoose.Schema({
+    nama: {type:String, required:true},
+    email: {type:String, required:true, unique:true},
+    password: {type:String, required:true},
+    riwayat: {type:Array, required:true, default: []},
+}))
+
+module.exports = User;
