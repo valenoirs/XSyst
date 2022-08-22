9ca5d9f287257c27844d767e163c51bf7e95985e
valenoirs <luckyvw.general@gmail.com>
2022-08-22 13:46:04 +0800

    update penyakit

--- controllers/solusi.js
+++ controllers/solusi.js
@@ -1,45 +1,72 @@
-const {v4:uuid} = require('uuid');
+const { v4: uuid } = require("uuid");
 
-const Solusi = require('../models/solusi');
+const Solusi = require("../models/solusi");
 
 module.exports.solusiAdd = async (req, res) => {
-    const solusi = await Solusi.find()
+  const solusi = await Solusi.find();
 
-    const solusiAdd = [{
-            id: uuid(),
-            rules: ['G01','G02','G03','G05','G06','G07','G08','G09'],
-            keterangan: 'Adapun pilihan terapi untuk kelainan refraksi, yaitu kacamata, kontak lensa dan bedah refraktif. Termasuk di dalamnya Laser Assisted In Situ Keratomileusis (Lasik), Trans-PRK, dan Clear lens extraction. Namun kebutuhan mengoreksi kelainan refraksi tergantung dari rules pasien.',
-        },
-        {
-            id: uuid(),
-            rules: ['G03','G04','G10','G11','G12','G13'],
-            keterangan: 'Pengobatan katarak dengan tujuan untuk memulihkan penglihatan adalah dengan cara mengeluarkan lensa yang mengalami kekeruhan, yaitu dengan tindakan operasi.',
-        },
-        {
-            id: uuid(),
-            rules: ['G04','G11','G14','G15','G16'],
-            keterangan: 'Penggunaan obat tetes mata baik dilakukan untuk mengurangi pembentukan cairan pada mata karena adanya penekanan. Selain penggunaan obat tetes mata, untuk kasus yang lebih parah dapat dilakukan tindakan operasi ataupun laser.',
-        },
-        {
-            id: uuid(),
-            rules: ['G01','G17','G18'],
-            keterangan: 'Retina yang sobek biasanya dapat diatasi dengan prosedur sederhana tanpa operasi (dilakukan di ruang periksa dokter). Tujuan pengobatan adalah mencegah retina menjadi lepas seutuhnya. Tapi untuk kasus yang parah harus diambil tindakan operasi. Sebaiknya segera melakukan pemeriksaan kepada ahli',
-        },
-        {
-            id: uuid(),
-            rules: ['G01','G04','G19','G20'],
-            keterangan: 'Pada konjungtivitis Antibiotik tunggal seperti gentamisisn, kloramfenikol, polimiksin dan sebagainya selama 3-5 hari atau tetes mata antibiotik spektrum tiap jam disertai salep mata 4-5 kali sehari.',
-        },
-    ]
+  const solusiAdd = [
+    {
+      id: uuid(),
+      rules: ["G01", "G02", "G03", "G05", "G07", "G08"],
+      keterangan:
+        "Pengobatan pada miopia dapat dipilih sesuai dengan usia, tingkat keparahan, serta kondisi pengidap. Jenis penanganan yang dilakukan bagi rabun jauh dapat berupa Operasi dengan sinar laser, Implantasi lensa buatan, Penggunaan kacamata atau lensa kontak.",
+    },
+    {
+      id: uuid(),
+      rules: ["G01", "G02", "G03", "G06", "G07"],
+      keterangan:
+        "Pengobatan pada miopia dapat dipilih sesuai dengan usia, tingkat keparahan, serta kondisi pengidap. Jenis penanganan yang dilakukan bagi rabun jauh dapat berupa Operasi dengan sinar laser, Implantasi lensa buatan, Penggunaan kacamata atau lensa kontak.",
+    },
+    {
+      id: uuid(),
+      rules: ["G01", "G03", "G08"],
+      keterangan:
+        "Pengobatan pada miopia dapat dipilih sesuai dengan usia, tingkat keparahan, serta kondisi pengidap. Jenis penanganan yang dilakukan bagi rabun jauh dapat berupa Operasi dengan sinar laser, Implantasi lensa buatan, Penggunaan kacamata atau lensa kontak.",
+    },
+    {
+      id: uuid(),
+      rules: ["G01", "G05", "G06", "G07", "G08", "G09"],
+      keterangan:
+        "Pengobatan pada miopia dapat dipilih sesuai dengan usia, tingkat keparahan, serta kondisi pengidap. Jenis penanganan yang dilakukan bagi rabun jauh dapat berupa Operasi dengan sinar laser, Implantasi lensa buatan, Penggunaan kacamata atau lensa kontak.",
+    },
+    {
+      id: uuid(),
+      rules: ["G03", "G04", "G10", "G11", "G12", "G13"],
+      keterangan:
+        "Pengobatan katarak dengan tujuan untuk memulihkan penglihatan adalah dengan cara mengeluarkan lensa yang mengalami kekeruhan, yaitu dengan tindakan operasi.",
+    },
+    {
+      id: uuid(),
+      rules: ["G04", "G11", "G14", "G15", "G16"],
+      keterangan:
+        "Penggunaan obat tetes mata baik dilakukan untuk mengurangi pembentukan cairan pada mata karena adanya penekanan. Selain penggunaan obat tetes mata, untuk kasus yang lebih parah dapat dilakukan tindakan operasi ataupun laser.",
+    },
+    {
+      id: uuid(),
+      rules: ["G01", "G17", "G18"],
+      keterangan:
+        "Retina yang sobek biasanya dapat diatasi dengan prosedur sederhana tanpa operasi (dilakukan di ruang periksa dokter). Tujuan pengobatan adalah mencegah retina menjadi lepas seutuhnya. Tapi untuk kasus yang parah harus diambil tindakan operasi. Sebaiknya segera melakukan pemeriksaan kepada ahli",
+    },
+    {
+      id: uuid(),
+      rules: ["G01", "G04", "G19", "G20"],
+      keterangan:
+        "Pada konjungtivitis Antibiotik tunggal seperti gentamisisn, kloramfenikol, polimiksin dan sebagainya selama 3-5 hari atau tetes mata antibiotik spektrum tiap jam disertai salep mata 4-5 kali sehari.",
+    },
+  ];
 
-    if(solusi.length === 0){{
-        Solusi.insertMany(solusiAdd).then(() => {
-            console.log('Solusi Added')
-        }).catch((error) => {
-            console.log(error)
+  if (solusi.length === 0) {
+    {
+      Solusi.insertMany(solusiAdd)
+        .then(() => {
+          console.log("Solusi Added");
+        })
+        .catch((error) => {
+          console.log(error);
         });
-    }}
-    else {
-        console.log("Solusi already in db")
     }
-}
+  } else {
+    console.log("Solusi already in db");
+  }
+};
--- index.js
+++ index.js
@@ -1,37 +1,40 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const expressLayouts = require('express-ejs-layouts')
-const session = require('express-session')
-const MongoStore = require('connect-mongo')
-const methodOverride = require('method-override')
-const flash = require('connect-flash')
-const helmet = require('helmet')
+const express = require("express");
+const mongoose = require("mongoose");
+const expressLayouts = require("express-ejs-layouts");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const methodOverride = require("method-override");
+const flash = require("connect-flash");
+const helmet = require("helmet");
 
 // Local module
-const penyakitController = require('./controllers/penyakit')
-const gejalaController = require('./controllers/gejala')
-const solusiController = require('./controllers/solusi')
+const penyakitController = require("./controllers/penyakit");
+const gejalaController = require("./controllers/gejala");
+const solusiController = require("./controllers/solusi");
 
-require('dotenv').config()
+require("dotenv").config();
 
-const app = express()
-const port = process.env.PORT || 5000
+const app = express();
+const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(helmet({
+app.use(
+  helmet({
     crossOriginEmbedderPolicy: false,
     contentSecurityPolicy: {
-        directives: {
-            scriptSrc: [
-                "'self'",
-                'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
-                'https://unpkg.com/aos@2.3.1/dist/aos.js'
-            ],
-        }
-    }
-}))
+      directives: {
+        scriptSrc: [
+          "'self'",
+          "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js",
+          "https://unpkg.com/aos@2.3.1/dist/aos.js",
+        ],
+      },
+    },
+  })
+);
 
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
@@ -40,53 +43,55 @@ app.use(session({
     //     collectionName: 'sessions'
     // }),
     cookie: {
-        maxAge: 1000 * 60 * 60 * 24 // 1 day
-    }
-}))
+      maxAge: 1000 * 60 * 60 * 24, // 1 day
+    },
+  })
+);
 
 app.use((req, res, next) => {
-    if(req.session.idUser && req.session.namaUser && req.session.emailUser){
-        res.locals.idUser = req.session.idUser
-        res.locals.namaUser = req.session.namaUser
-        res.locals.emailUser = req.session.emailUser
-    }
+  if (req.session.idUser && req.session.namaUser && req.session.emailUser) {
+    res.locals.idUser = req.session.idUser;
+    res.locals.namaUser = req.session.namaUser;
+    res.locals.emailUser = req.session.emailUser;
+  }
 
-    next()
-})
+  next();
+});
 
-app.use(flash())
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-app.use(methodOverride('_method'))
+app.use(flash());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride("_method"));
 
 // Templating Engine
-app.set('view engine', 'ejs')
-app.use(expressLayouts)
+app.set("view engine", "ejs");
+app.use(expressLayouts);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    penyakitController.penyakitAdd()
-    gejalaController.gejalaAdd()
-    solusiController.solusiAdd()
-    console.log(`Successfuly connected to MongoDB Atlass`)
-})
-.catch((error) => console.log(error))
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    penyakitController.penyakitAdd();
+    gejalaController.gejalaAdd();
+    solusiController.solusiAdd();
+    console.log(`Successfuly connected to MongoDB Atlass`);
+  })
+  .catch((error) => console.log(error));
 
 // HTTP Routes
-app.use('/', require('./routes/user'))
+app.use("/", require("./routes/user"));
 
 // Ping Server!
-app.get('/ping', (req, res, next) => {
-    res.send('pong!')
-})
+app.get("/ping", (req, res, next) => {
+  res.send("pong!");
+});
 
-app.use('/', (req, res) => {
-    res.status(404)
-    res.send('<h1>404</h1>')
-})
+app.use("/", (req, res) => {
+  res.status(404);
+  res.send("<h1>404</h1>");
+});
 
 app.listen(port, () => {
-    console.log(`Server running at port ${port}`)
-})
+  console.log(`Server running at port ${port}`);
+});
